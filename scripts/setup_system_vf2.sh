05b5bb6f1356b418d2faba12f3351d0fcee9f0a9
Frank Halasz <frank@halasz.org>
2024-03-26 19:07:40 +0000

    in VF2 script, change host name to zynia instead of zynthian

--- scripts/setup_system_vf2.sh
+++ scripts/setup_system_vf2.sh
@@ -532,7 +532,7 @@ cp -a $ZYNTHIAN_DATA_DIR/snapshots/* $ZYNTHIAN_MY_DATA_DIR/snapshots/000
 #Change Hostname
 if [ "$ZYNTHIAN_CHANGE_HOSTNAME" == "yes" ]; then
     echo "zynthian" > /etc/hostname
-    sed -i -e "s/127\.0\.1\.1.*$/127.0.1.1\tzynthian/" /etc/hosts
+    sed -i -e "s/127\.0\.1\.1.*$/127.0.1.1\tzynia/" /etc/hosts
 fi
 
 # Run configuration script
