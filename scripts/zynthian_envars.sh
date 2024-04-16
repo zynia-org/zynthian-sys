2800428ce0003bc3e1e6c5cdd154b6f8fbfeb991
Frank Halasz <frank@halasz.org>
2024-04-16 21:53:12 +0000

    fix up envars

--- scripts/zynthian_envars.sh
+++ scripts/zynthian_envars.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #******************************************************************************
 # ZYNTHIAN PROJECT: Zynthian Environment Vars
-# 
+#
 # Setup Zynthian Environment Variables
-# 
+#
 # Copyright (C) 2015-2016 Fernando Moyano <jofemodo@zynthian.org>
 #
 #******************************************************************************
-# 
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
 # published by the Free Software Foundation; either version 2 of
