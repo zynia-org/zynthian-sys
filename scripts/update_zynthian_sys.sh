385c0c850c84afedf7d8ecfbe93f2f6fe6de1375
Frank Halasz <frank@halasz.org>
2024-06-16 06:24:56 +0000

    Add riscv64 case to fixup of LV2_PATH

--- scripts/update_zynthian_sys.sh
+++ scripts/update_zynthian_sys.sh
@@ -303,6 +303,10 @@ if [ ${MACHINE_HW_NAME} = "armv7l" ]; then
 	arch_libdir="arm-linux-gnueabih"
 elif [ ${MACHINE_HW_NAME} = "aarch64" ]; then
 	arch_libdir="aarch64-linux-gnu"
+elif [ ${MACHINE_HW_NAME} = "riscv64" ]; then
+	arch_libdir="riscv64-linux-gnu"
+else
+        arch_libdir="unknown"
 fi
 LV2_PATH="/usr/lib/lv2:/usr/lib/$arch_libdir/lv2:/usr/local/lib/lv2:/usr/local/lib/$arch_libdir/lv2:$ZYNTHIAN_PLUGINS_DIR/lv2:$ZYNTHIAN_DATA_DIR/presets/lv2:$ZYNTHIAN_MY_DATA_DIR/presets/lv2"
 LV2_PATH_ESC=${LV2_PATH//\//\\\/}
